bbe3f7c368f717932c69f0b8e0bea0bd2a8dbb77
Martin Thuranira <38188355+MartinThuranira@users.noreply.github.com>
2021-03-15 18:46:03 +0300

    Update main.rs
    added impl for Rectangle

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,15 +1,23 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
     }
+    impl Rectangle {
+        fn perimeter(&self) -> u32 {
+            let out = self.width+self.height;
+            out *2
+
+        }
+        }
     fn main() {
-    let rect1 = Rectangle {
+    let mut rect1 = Rectangle {
     width: 30,
     height: 50,
     };
     println!(
-    "The area of the rectangle is {} square pixels.",
-    area(&rect1)
+    "The area of the rectangle is {} square pixels and perimeter is {:#?}",
+    area(&rect1),&rect1.perimeter()
     );
     }
     fn area(rectangle: &Rectangle) -> u32 {
